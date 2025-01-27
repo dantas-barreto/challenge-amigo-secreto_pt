11962ba83a19673fd9e0f0830acd5478267808b8
João Pedro Souza Dantas Barreto <jpsdb10@hotmail.com>
2025-01-27 14:30:32 -0300

    Função para agregar amigos implementada

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let amigos = [];
+
+function adicionarAmigo() {
+    amigo = document.querySelector('input').value;
+    if (amigo == "") {
+        alert('Por favor, insira um nome.');
+    } else {
+        amigos.push(amigo);
+    }
+        limparCampo();
+}
+
+function limparCampo() {
+    amigo = document.querySelector('input');
+    amigo.value = '';
+}
